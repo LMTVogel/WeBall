f39431e91373f53dedd0a8fad7d70e8a3121ee36
0xlaurens <lauwiv@gmail.com>
2024-06-21 15:37:43 +0200

    fix -> update signatures

--- LogisticsManagement/LogisticsManagement.DomainServices/Interfaces/IRepository.cs
+++ LogisticsManagement/LogisticsManagement.DomainServices/Interfaces/IRepository.cs
@@ -4,9 +4,9 @@ namespace LogisticsManagement.DomainServices.Interfaces;
 
 public interface IRepository<T> where T : class
 {
-    Task<LogisticsCompany?> GetByIdAsync(Guid id);
+    Task<T?> GetByIdAsync(Guid id);
     Task<List<T>> GetAllAsync();
     Task CreateAsync(T entity);
-    Task<LogisticsCompany?> UpdateAsync(Guid id, T entity);
+    Task<T?> UpdateAsync(Guid id, T entity);
     Task DeleteAsync(Guid id);
 }
