6bce959b524670f97f0e7875c299ca1bd3594f57
0xlaurens <lauwiv@gmail.com>
2024-06-22 14:28:08 +0200

    Payments -> mock impl

--- PaymentManagement/PaymentManagement.Infrastructure/Payments/ForwardPaymentProcessor.cs
+++ PaymentManagement/PaymentManagement.Infrastructure/Payments/ForwardPaymentProcessor.cs
@@ -3,15 +3,24 @@ using PaymentManagement.DomainServices.Interfaces;
 
 namespace PaymentManagement.Infrastructure.Payments;
 
-public class ForwardPaymentProcessor: IPaymentProcessor
+public class ForwardPaymentProcessor : IPaymentProcessor
 {
-    public Task<PaymentStatus> ProcessPaymentAsync(Guid paymentId)
+    public async Task<PaymentStatus> ProcessPaymentAsync(Guid paymentId)
     {
-        throw new NotImplementedException();
+        var msMockDelay = new Random().Next(200, 400);
+        await Task.Delay(msMockDelay);
+
+        var status = new Random().Next(0, 100);
+        return status switch
+        {
+            < 90 => PaymentStatus.Paid,
+            _ => PaymentStatus.Failed,
+        };
     }
 
-    public Task CancelPaymentAsync(Guid paymentId)
+    public async Task CancelPaymentAsync(Guid paymentId)
     {
-        throw new NotImplementedException();
+        var msMockDelay = new Random().Next(200, 400);
+        await Task.Delay(msMockDelay);
     }
 }
